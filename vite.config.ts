be91c83109df5f870d7e4c00f84e9603829ef25b
iOzzie <38383678+iOzzie@users.noreply.github.com>
2026-05-10 06:57:09 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Change this to match your EXACT repository name
   base: '/comfyui-image-parser/', 
 })
